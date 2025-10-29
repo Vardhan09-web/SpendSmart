3e90ff2a7f715d842c36ff9abc4e5757f1249cbf
Vardhan Malyala <vardhanmalyala@gmail.com>
2025-10-30 00:13:54 +0530

    conflicts resolve

--- Front-end/my-react-app/src/App.jsx
+++ Front-end/my-react-app/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
         Tailwind is Working 
       </h1>
       <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
-        Click Me
+        Click 
       </button>
     </div>
   );
